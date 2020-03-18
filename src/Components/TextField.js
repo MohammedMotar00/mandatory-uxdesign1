ec77c78e49c0fa37abd5e54ae0a0ff7b68341968
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-03-18 16:23:59 +0100

    försöker hitta logik för klicka i input o utanför!

--- src/Components/TextField.js
+++ src/Components/TextField.js
@@ -2,15 +2,46 @@ import React, { Component } from 'react'
 import '../Css/InputFiled.css';
 
 class TextField extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            clicked: false
+        }
+
+        this.onClick = React.createRef();
+    }
+
+    componentDidMount(){
+        document.addEventListener('click', this.clickOutsideOfInput)
+    }
+
+    componentWillUnmount(){
+        document.addEventListener('click', this.clickOutsideOfInput)
+    }
+
+    clickOutsideOfInput = (e) =>{
+        let Class = '';
+        // console.log(e.target, 'hej');
+        console.log(this.onClick.current);
+        if(this.onClick.current === e.target){
+            this.setState({clicked: !this.state.clicked})
+            console.log('true');
+            Class = 'clicked'
+        }
+        else {
+        this.setState({clicked: false})
+        console.log('false');
+        Class = ''
+        }
+    }
+
     render() {
+        const { clicked } = this.state;
+
         return (
-            // <div>
-            //     <label data-placeholder="Name">
-            //         <input type="text" className="clicked" />
-            //     </label>
-            // </div>
-            <div className="clicked" data-placeholder="s">
-                <input type="text" />
+            <div className={Class} data-placeholder="Name">
+                <input type="text" className={Class} ref={this.onClick} onClick={this.clickOutsideOfInput} />
             </div>
         )
     }
