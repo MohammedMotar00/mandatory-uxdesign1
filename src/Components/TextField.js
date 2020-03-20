c8addb0ba68e446a2da7540040e37a0ebfeceff9
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-03-20 16:14:23 +0100

    fixade input fältet! den är klar!

--- src/Components/TextField.js
+++ src/Components/TextField.js
@@ -1,44 +1,18 @@
 import React, { Component } from 'react'
 import '../Css/InputFiled.css';
 
-let Class = '';
-
-class TextField extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            clicked: false
-        }
-
-        this.onClick = React.createRef();
-    }
-
-    componentDidMount(){
-        document.addEventListener('click', this.clickOutsideOfInput)
-    }
-
-    componentWillUnmount(){
-        document.addEventListener('click', this.clickOutsideOfInput)
-    }
-
-    clickOutsideOfInput = (e) =>{
-        if(this.onClick.current === e.target){
-            this.setState({clicked: !this.state.clicked})
-            Class = 'clicked'
-        }
-        else {
-            Class = ''
-            this.setState({clicked: false})
-        }
-    }
+export class TextField extends Component {
     render() {
         return (
-            <div className={Class} data-placeholder="Name" >
-                <input type="text" id="textField" className={Class} ref={this.onClick} onClick={this.clickOutsideOfInput} />
+            <div className="container-textField">
+                <input type="text" className="input-textField" required />
+                <label className="control-placeholder">Label</label>
+                <span class="focus-border"></span>
             </div>
         )
     }
 }
+
+
 
 export default TextField
